8a0852979f336775d48b476e83e35e5df653581b
LucDorpmans <GitHub@sure.nl>
2024-09-29 18:22:38 +0200

    Upd scripts

--- GetMy-55341-Scripts.ps1
+++ GetMy-55341-Scripts.ps1
@@ -1,14 +1,8 @@
-# GetMyScripts.ps1
-Function Get-MyScript { Param( [string]$AFile,[switch]$EditFile = $False, 
+# GetMy-55341-Scripts.ps1
+Function Get-MyScript { Param( [string]$AScript,[switch]$EditFile = $False, 
 							   [string]$SPath = "$env:USERPROFILE\Downloads\")
-			Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/My-55039-Repo/main/$AFile"  -Outfile "$SPath$AFile" 
-			If ($EditFile) { PSEdit  ("$SPath$AFile" )} }
-		
-Get-MyScript "55039-Install-Modules.ps1" 
+			Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/My-Base-Repo/main/$AScript"  -Outfile "$SPath$AScript" 
+If ($EditFile) { PSEdit ("$SPath$AScript" )} }
 
-# Get Install-VSCode script"
-# Invoke-Webrequest -Uri "https://raw.githubusercontent.com/PowerShell/vscode-powershell/master/scripts/Install-VSCode.ps1" -Outfile "$env:USERPROFILE\Downloads\Install-VSCode.ps1"
-# PSEdit  ("$env:USERPROFILE\Downloads\Install-VSCode.ps1")
 Invoke-WebRequest -uri "https://github.com/LucDorpmans/$MyRepo/archive/refs/heads/main.zip" -OutFile "$env:USERPROFILE\Downloads\$MyRepo.zip"
 Expand-Archive "$env:USERPROFILE\Downloads\$MyRepo.zip" -DestinationPath C:\ -Force
-
